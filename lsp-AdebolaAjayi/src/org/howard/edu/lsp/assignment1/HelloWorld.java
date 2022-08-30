7a3a61503cde9e1d5c263034545e752cd4a8b6ff
adebolaajayi <adebolaajayi@172.17.2.252>
2022-08-30 17:14:18 -0400

    This is my commit for assignment 1.

---
+++ lsp-AdebolaAjayi/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,17 @@
+package org.howard.edu.lsp.assignment1;
+
+/*
+ * Adebola Ajayi
+ * 
+ * @adebolaaj
+ * 
+ * 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
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello LSP!");
+
+	}
+
+}
