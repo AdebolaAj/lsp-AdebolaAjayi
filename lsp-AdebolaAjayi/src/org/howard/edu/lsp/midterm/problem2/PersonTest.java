6e795e0d329ec7e56689389ede5b0e9dcb6263ba
adebolaajayi <adebolaajayi@172.17.2.252>
2022-10-27 21:44:01 -0400

    midterm first commit

--- lsp-AdebolaAjayi/src/org/howard/edu/lsp/midterm/problem2/PersonTest.java
+++ lsp-AdebolaAjayi/src/org/howard/edu/lsp/midterm/problem2/PersonTest.java
@@ -1,5 +1,25 @@
 package org.howard.edu.lsp.midterm.problem2;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.howard.edu.lsp.midterm.problem1.VotingMachine;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
 public class PersonTest {
+	@Test
+	@DisplayName("PersonTest.toString Test case")
+	public void testToString() {
+		Person person = new Person("Adebola", "111-11-1111", 20);
+		
+		assertEquals("Adebola, 20, 111-11-1111", person.toString());
+	}
+	
 
 }
